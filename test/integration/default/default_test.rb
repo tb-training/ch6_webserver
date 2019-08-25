9e490f662f1fd78d429b5276e95df586d81600c2
dev1 <tio.bagio@gmail.com>
2019-08-25 20:00:23 +0800

    add test case

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -13,8 +13,8 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(8080) do
+  it { should be_listening }
 end
 
 ## case insensitive match
